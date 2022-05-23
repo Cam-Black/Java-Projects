159ccb5d488f8401c343d35e703fc960cf4723ef
Cam-Black <cameronablack1998@gmail.com>
2022-05-23 11:52:00 +0100

    Created boilerplate for calculating results and percentage of results

---
+++ Results/src/com/qa/results/FormatData.java
@@ -0,0 +1,16 @@
+package com.qa.results;
+
+public class FormatData {
+     private String physics = "Physics";
+     private String chemistry = "Chemistry";
+     private String biology = "Biology";
+     private int total;
+     private int percentage;
+    
+    public FormatData(String physics, String chemistry, String biology) {
+        this.physics = physics;
+        this.chemistry = chemistry;
+        this.biology = biology;
+    }
+         
+}
