5fa121422a7f1c286d80ada508fed35d87823da0
Cam-Black <cameronablack1998@gmail.com>
2022-05-23 12:59:04 +0100

    Setup calculate class with a method to display results in a nice way using separate private methods

--- Results/src/DisplayResults.java
+++ Results/src/DisplayResults.java
@@ -1,5 +1,10 @@
+import com.qa.results.CalculateResults;
+
 public class DisplayResults {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CalculateResults jeremy = new CalculateResults("Jeremy", 100, 120, 132);
+        System.out.println(jeremy.displayResults());
+        CalculateResults steve = new CalculateResults("Steve", 99, 84, 135);
+        System.out.println(steve.displayResults());
     }
 }
--- Results/src/com/qa/results/CalculateResults.java
+++ Results/src/com/qa/results/CalculateResults.java
@@ -1,18 +1,40 @@
 package com.qa.results;
 
 public class CalculateResults {
-     private String physicsExamScore = "Physics";
-     private String chemistryExamScore = "Chemistry";
-     private String biologyExamScore = "Biology";
-     private int physicsExamTotalMarks = 150;
-     private int chemistryExamTotalMarks = 150;
-     private int biologyExamTotalMarks = 150;
-     private int total;
-     private int percentage;
     
-    public CalculateResults(String physicsScore, String chemistryScore, String biologyScore) {
+    private String name;
+    private int physicsExamScore;
+    private int chemistryExamScore;
+    private int biologyExamScore;
+    private int physicsExamTotalMarks = 150;
+    private int chemistryExamTotalMarks = 150;
+    private int biologyExamTotalMarks = 150;
+    private int total;
+    private int percentage;
+    
+    public CalculateResults(String name, int physicsScore, int chemistryScore, int biologyScore) {
+        this.name = name;
         this.physicsExamScore = physicsScore;
         this.chemistryExamScore = chemistryScore;
         this.biologyExamScore = biologyScore;
+    }
+    private String addApostrophe() {
+        String apostropheName = this.name += "'s";
+        return apostropheName;
+    }   
+        
+    private String biologyResults() {
+        return "Biology: " + this.biologyExamScore + " out of " + this.biologyExamTotalMarks;
+    }
+
+    private String chemistryResults() {
+        return "Chemistry: " + this.chemistryExamScore + " out of " + this.chemistryExamTotalMarks;
+    }
+    private String physicsResults() {
+        return "Physics: " + this.physicsExamScore + " out of " + this.physicsExamTotalMarks;
+    }
+
+    public String displayResults() {
+        return this.addApostrophe() + " results:\n" + this.biologyResults() + "\n" + this.chemistryResults() + "\n" + this.physicsResults();
     }
 }
