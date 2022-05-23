e717ae9b78378cba34b0d1fcbb7c08fa403c20aa
Cam-Black <cameronablack1998@gmail.com>
2022-05-23 11:39:20 +0100

    Removed getters and setters from Operations class, not needed, formatted whitespcae for better readability

--- Calculator/src/com/qa/calculator/Calculator.java
+++ Calculator/src/com/qa/calculator/Calculator.java
@@ -5,8 +5,10 @@ public class Calculator {
     public static void main(String[] args) throws Exception {
         Operations myNumbers = new Operations(2, 5);
         Operations myNumbers2 = new Operations(12, 34);
+
         System.out.println(myNumbers.addTwoNumbers());
         System.out.println(myNumbers2.addTwoNumbers());
+        
         System.out.println(myNumbers.multiplyTwoNumbers());
         System.out.println(myNumbers.subtractTwoNumbers());
         System.out.println(myNumbers.divideTwoNumbers());
--- Calculator/src/com/qa/calculator/Operations.java
+++ Calculator/src/com/qa/calculator/Operations.java
@@ -9,34 +9,16 @@ public class Operations {
         this.num2 = num2;
     }
 
-    public void setMyNumbers(int num1, int num2) {
-        this.num1 = num1;
-        this.num2 = num2;
-    }
-    public int getNum1() {
-        return num1;
-    }
-
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
-
-    public int getNum2() {
-        return num1;
-    }
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
-    }
-
     public String addTwoNumbers() {
         int result = this.num1 + this.num2;
         return this.num1 + " + " + this.num2 + " = " + result;
     }
+
     public String multiplyTwoNumbers() {
         int result = this.num1 * this.num2;
         return this.num1 + " * " + this.num2 + " = " + result;
     }
+    
     public String subtractTwoNumbers() {
         int result = this.num1 - this.num2;
         return this.num1 + " - " + this.num2 + " = " + result;
