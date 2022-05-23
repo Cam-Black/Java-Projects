80d285abf39a7709d8cf8ef5cf76eb183e96a020
Cam-Black <cameronablack1998@gmail.com>
2022-05-23 14:49:33 +0100

    Finished the results challenge

--- Results/src/DisplayResults.java
+++ Results/src/DisplayResults.java
@@ -4,7 +4,8 @@ public class DisplayResults {
     public static void main(String[] args) throws Exception {
         CalculateResults jeremy = new CalculateResults("Jeremy", 100, 120, 132);
         System.out.println(jeremy.displayResults());
-        CalculateResults steve = new CalculateResults("Steve", 99, 84, 135);
+        
+        CalculateResults steve = new CalculateResults("Steve'", 99, 84, 135);
         System.out.println(steve.displayResults());
     }
 }
--- Results/src/com/qa/results/CalculateResults.java
+++ Results/src/com/qa/results/CalculateResults.java
@@ -9,8 +9,8 @@ public class CalculateResults {
     private int physicsExamTotalMarks = 150;
     private int chemistryExamTotalMarks = 150;
     private int biologyExamTotalMarks = 150;
-    private int total;
-    private int percentage;
+    private double total;
+    private double percentage;
     
     public CalculateResults(String name, int physicsScore, int chemistryScore, int biologyScore) {
         this.name = name;
@@ -18,9 +18,16 @@ public class CalculateResults {
         this.chemistryExamScore = chemistryScore;
         this.biologyExamScore = biologyScore;
     }
+    
     private String addApostrophe() {
-        String apostropheName = this.name += "'s";
-        return apostropheName;
+        String apostropheName = this.name;
+
+        if (apostropheName.substring(apostropheName.length() - 1 ).equals("'"))  {
+            return apostropheName += "s";
+        }
+        else {
+            return apostropheName += "'s";
+        }
     }   
         
     private String biologyResults() {
@@ -30,11 +37,19 @@ public class CalculateResults {
     private String chemistryResults() {
         return "Chemistry: " + this.chemistryExamScore + " out of " + this.chemistryExamTotalMarks;
     }
+    
     private String physicsResults() {
         return "Physics: " + this.physicsExamScore + " out of " + this.physicsExamTotalMarks;
     }
-
-    public String displayResults() {
-        return this.addApostrophe() + " results:\n" + this.biologyResults() + "\n" + this.chemistryResults() + "\n" + this.physicsResults();
+    
+    private double totalPercentage() {
+        this.total = biologyExamScore + chemistryExamScore + physicsExamScore;
+        double totalMarks = biologyExamTotalMarks + chemistryExamTotalMarks + physicsExamTotalMarks;
+        this.percentage = (total * 100) / totalMarks;
+        double twoDecimalPlaces = Math.floor(this.percentage * 100) / 100;
+        return twoDecimalPlaces;
     }
+    public String displayResults() {
+        return this.addApostrophe() + " results:\n" + this.biologyResults() + "\n" + this.chemistryResults() + "\n" + this.physicsResults() + "\n" + "Overall Percentage: " + this.totalPercentage() + "%";
+    } 
 }
