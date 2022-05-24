aee0b6614bea79daecf00cbb21605749eee40c2f
Cam-Black <cameronablack1998@gmail.com>
2022-05-24 16:24:55 +0100

    Created a project for using conditionals and iterators

---
+++ Iterations/src/com/qa/iterators/Runner.java
@@ -0,0 +1,11 @@
+package com.qa.iterators;
+
+public class Runner {
+    public static void main(String[] args) throws Exception {
+        Flowchart myFlowchart = new Flowchart();
+        // myFlowchart.whileALessThan200();
+        // myFlowchart.whileAIsLessThanEqualTo200();
+        FizzBuzz printOut = new FizzBuzz();
+        printOut.fizzBuzz();
+    }
+}
