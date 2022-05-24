cd56e6ff615d794ef7e0145445eeaca8e3dc99dc
Cam-Black <cameronablack1998@gmail.com>
2022-05-24 09:21:04 +0100

    Added comments for each method

--- Results/src/com/qa/results/CalculateResults.java
+++ Results/src/com/qa/results/CalculateResults.java
@@ -12,6 +12,7 @@ public class CalculateResults {
     private double total;
     private double percentage;
     
+    //Constructor to create a pool of a result for a user
     public CalculateResults(String name, int physicsScore, int chemistryScore, int biologyScore) {
         this.name = name;
         this.physicsExamScore = physicsScore;
@@ -19,6 +20,7 @@ public class CalculateResults {
         this.biologyExamScore = biologyScore;
     }
     
+    //Add an 's to the end of a user's name if they don't already have one
     private String addApostrophe() {
         String apostropheName = this.name;
 
@@ -28,7 +30,8 @@ public class CalculateResults {
         
         return apostropheName;
     }   
-        
+
+    //Return the marks out of x for each Science Exam
     private String biologyResults() {
         return "Biology: " + this.biologyExamScore + " out of " + this.biologyExamTotalMarks;
     }
@@ -41,10 +44,12 @@ public class CalculateResults {
         return "Physics: " + this.physicsExamScore + " out of " + this.physicsExamTotalMarks;
     }
     
+    //Round results to 2 Decimal Places
     private double roundToTwoDecimalPlaces(double num) {
         return num = Math.floor(num * 100) / 100;
     }
 
+    //Calculates the overall percentage of the results
     private double totalPercentage() {
         this.total = biologyExamScore + chemistryExamScore + physicsExamScore;
         double totalMarks = biologyExamTotalMarks + chemistryExamTotalMarks + physicsExamTotalMarks;
@@ -52,6 +57,7 @@ public class CalculateResults {
         return this.roundToTwoDecimalPlaces(this.percentage);
     }
 
+    //Calculate percentage of the exam results for each Science exam
     private double calculateBiologyPercentage() {
         double bioPercentage = (biologyExamScore * 100.0) / biologyExamTotalMarks;
         return roundToTwoDecimalPlaces(bioPercentage);
@@ -67,10 +73,12 @@ public class CalculateResults {
         return this.roundToTwoDecimalPlaces(physicsPercentage);
     }
 
+    //Check if user failed any of the exams
     private boolean checkExamPassOrFail() {
         return calculateBiologyPercentage() < 60 || calculateChemistryPercentage() < 60 || calculatePhysicsPercentage() < 60;
     }
     
+    //Check if the user passed their exams overall
     private boolean checkOverallPassOrFail() {
         if(totalPercentage() < 60 || !this.checkExamPassOrFail()) {
             return false;
@@ -78,6 +86,7 @@ public class CalculateResults {
         return true;
     }
 
+    //Return number of exams user failed
     private String failedExams() {
         if (calculateBiologyPercentage() < 60 && calculateChemistryPercentage() >= 60 && calculatePhysicsPercentage() >= 60 || calculateChemistryPercentage() < 60 && calculateBiologyPercentage() >= 60 && calculatePhysicsPercentage() >= 60 || calculatePhysicsPercentage() < 60 && calculateBiologyPercentage() >= 60 && calculateChemistryPercentage() >= 60) {
             return "You failed 1 Exam!";
@@ -90,6 +99,7 @@ public class CalculateResults {
         }
     }
 
+    //Method to display the output in a neat and concise format for good readability
     public String displayResults() {
         if (!checkOverallPassOrFail()) {
             return this.addApostrophe() + " results:\n" + this.biologyResults() + "\n" + this.chemistryResults() + "\n" + this.physicsResults() + "\n" + "Overall Percentage: " + this.totalPercentage() + "% = Fail\n" + this.failedExams();
