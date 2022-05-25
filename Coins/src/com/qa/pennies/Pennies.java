999928be5f5ec2b3a49f616578f72e41756c0777
Cam-Black <cameronablack1998@gmail.com>
2022-05-25 16:24:25 +0100

    Added all projects/challenges completed today

--- Coins/src/com/qa/pennies/Pennies.java
+++ Coins/src/com/qa/pennies/Pennies.java
@@ -3,6 +3,6 @@ package com.qa.pennies;
 public class Pennies {
     public static void main(String[] args) {
         CalculateChange myChange = new CalculateChange();
-        System.out.println(myChange.returnChange(3.58, 10));
+        System.out.println(myChange.changeInCoins(21.53, 54.95));
     }
 }
