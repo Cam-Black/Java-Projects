c6dad761039f62d34927df05794049f95edb2b1b
Cam-Black <cameronablack1998@gmail.com>
2022-05-25 11:09:20 +0100

    Created ArrayList methods to add, get, set, and called the various methods in the runner

--- Arrays/src/com/qa/arraylists/Runner.java
+++ Arrays/src/com/qa/arraylists/Runner.java
@@ -2,6 +2,19 @@ package com.qa.arraylists;
 
 public class Runner {
     public static void main(String[] args) {
-        
+        ArrayLists dogBreeds = new ArrayLists();
+        dogBreeds.addToArrayList("Springer Spaniel");
+        dogBreeds.addToArrayList("Cocker Spaniel");
+        dogBreeds.addToArrayList("German Shepherd");
+        dogBreeds.displayMyStringArr();
+        dogBreeds.getElement(1);
+        dogBreeds.setElement(1, "Chihuahua");
+        dogBreeds.removeElement(1);
+        dogBreeds.displayMyStringArr();
+
+        ArrayLists myNumbers = new ArrayLists();
+        myNumbers.oneToTwenty();
+        myNumbers.displayMyIntArr();
+        myNumbers.squareAndCubeMyIntArr();
     }
 }
