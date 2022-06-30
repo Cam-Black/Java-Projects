b819456d6300d86f61fd440008917a7cdf16efa2
Cam-Black <cameronablack1998@gmail.com>
2022-06-30 20:46:15 +0100

    Added failing test to check empty shopping basket is 0

--- tddpractice/src/test/java/com/qa/basket/ShoppingBasketTest.java
+++ tddpractice/src/test/java/com/qa/basket/ShoppingBasketTest.java
@@ -1,4 +1,15 @@
 package com.qa.basket;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShoppingBasketTest {
+
+    @Test
+    public void totalOfEmptyBasket() {
+        ShoppingBasket basket = new ShoppingBasket();
+        assertEquals(0.0, basket.getTotal(), 0.0);
+
+    }
 }
