2a2716e036fc696b64aa167bf40840c081dd8942
Cam-Black <cameronablack1998@gmail.com>
2022-06-30 23:43:45 +0100

    Created passing test to calculate the running total of a list of Items

--- tddpractice/src/main/java/com/qa/basket/ShoppingBasket.java
+++ tddpractice/src/main/java/com/qa/basket/ShoppingBasket.java
@@ -1,7 +1,20 @@
 package com.qa.basket;
 
+import com.qa.basket.item.Item;
+
+import java.util.List;
+
 public class ShoppingBasket {
+    private List<Item> items;
+    public ShoppingBasket(){}
+    public ShoppingBasket(List<Item> items) {
+        this.items = items;
+    }
     public double getTotal() {
-        return 0.0;
+        double runningTotal =
+                items.stream()
+                        .map(x -> x.getCost())
+                        .reduce(0.0, (a,b) -> a+b);
+        return runningTotal;
     }
 }
---
+++ tddpractice/src/main/java/com/qa/basket/item/Item.java
@@ -0,0 +1,27 @@
+package com.qa.basket.item;
+
+/**
+ * Item class to allow creation of unique Items with a name, price and quantity.
+ * These can then be created individually or added to a collection type such as a list.
+ * In ShoppingBasket we utilise a list to add multiple items to our basket.
+ */
+public class Item {
+    //Attribute to set the name of an Item
+    private String name = "";
+    //Attribute to set the cost in GBP of an Item
+    private double cost = 0;
+    //Attribute to set the quantity of items to be added
+    private int quantity = 0;
+
+    public Item() {}
+
+    public Item(String name, double cost, int quantity) {
+        this.name = name;
+        this.cost = cost;
+        this.quantity = quantity;
+    }
+
+    public double getCost() {
+        return this.cost * (double) this.quantity;
+    }
+}
