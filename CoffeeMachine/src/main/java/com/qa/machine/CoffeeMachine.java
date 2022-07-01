7ac16495181a164e36274ac1b41526999a45ff3c
Cam-Black <cameronablack1998@gmail.com>
2022-07-01 13:39:55 +0100

    Created basic UML to model project from with Classes and methods declared in project

--- CoffeeMachine/src/main/java/com/qa/machine/CoffeeMachine.java
+++ CoffeeMachine/src/main/java/com/qa/machine/CoffeeMachine.java
@@ -1,6 +1,19 @@
 package com.qa.machine;
 
+import com.qa.machine.drink.Drinks;
+
 public class CoffeeMachine {
     private double startCredit = 0;
-    private java.util.ArrayList<String> drinks;
+    private java.util.ArrayList<Drinks> drinks;
+
+    public Drinks orderDrink(String drinkName) {
+        return null;
+    }
+    public double addCredit(double creditToAdd) {
+        return 0;
+    }
+
+    public double getCredit() {
+        return 0;
+    }
 }
---
+++ CoffeeMachine/src/main/java/com/qa/machine/drink/Drinks.java
@@ -0,0 +1,29 @@
+package com.qa.machine.drink;
+
+public class Drinks {
+    private String name = "";
+    private double price = 0;
+    private int quantity = 1;
+
+    public Drinks() {
+    }
+
+    public Drinks(String name, double price, int quantity) {
+    }
+
+    public double getPrice() {
+        return 0;
+    }
+
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    public String getName() {
+        return null;
+    }
+    @Override
+    public String toString() {
+        return null;
+    }
+}
