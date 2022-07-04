c7b86e7a41af291fb6df9368fbf1b18b78e46f63
Cam-Black <cameronablack1998@gmail.com>
2022-07-04 13:59:17 +0100

    renamed file

--- Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/controller/TSwiftAlbums.java
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/controller/TSwiftAlbums.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class TSwiftAlbums {
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello World!";
