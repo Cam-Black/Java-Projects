a20e98e402bc3534f391d6a00f218c05c159910a
Cam-Black <cameronablack1998@gmail.com>
2022-07-04 14:18:08 +0100

    renamed file to be about Middle Earth

--- Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/controller/MiddleEarth.java
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/controller/MiddleEarth.java
@@ -1,16 +1,23 @@
 package com.qa.springstarter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TSwiftAlbums {
+public class MiddleEarth {
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello World!";
     }
+
     @GetMapping("/goodbye")
     public String goodbyeWorld() {
         return "Goodbye World!";
+    }
+
+    @GetMapping("/isengard")
+    public String isengard() {
+        return "They're taking the Hobbits to Isengard!";
     }
 }
