a88fb81c091dfd4cdca531ffd189bc0c423bc3ba
Cam-Black <cameronablack1998@gmail.com>
2022-07-05 16:34:05 +0100

    Defined all methods from the HobbitService interface.

--- Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/shire/service/HobbitServiceList.java
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/shire/service/HobbitServiceList.java
@@ -1,7 +1,43 @@
 package com.qa.springstarter.shire.service;
 
 import com.qa.springstarter.service.HobbitService;
+import com.qa.springstarter.shire.Hobbit;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class HobbitServiceList implements HobbitService {
+    private List<Hobbit> hobbits = new ArrayList<>();
 
+    @Override
+    public Hobbit getAHobbit(int id) {
+        return hobbits.get(id);
+    }
+
+    @Override
+    public List<Hobbit> getAllHobbits() {
+        return hobbits;
+    }
+
+    @Override
+    public Hobbit createHobbit(Hobbit hobbit) {
+        this.hobbits.add(hobbit);
+        return this.hobbits.get(this.hobbits.size() - 1);
+    }
+
+    @Override
+    public Hobbit updateHobbit(int id, String forename, String familyName, int age) {
+        Hobbit updated = hobbits.get(id);
+        updated.setForename(forename);
+        updated.setFamilyName(familyName);
+        updated.setAge(age);
+        return updated;
+    }
+
+    @Override
+    public void deleteHobbit(int id) {
+        hobbits.remove(id);
+    }
 }
