19822c7a6e8d3156886cec16318e5850e6f34116
Cam-Black <cameronablack1998@gmail.com>
2022-07-05 16:35:23 +0100

    Fixed methods to call a service variable that calls HobbitService methods and set correct HTTP Status responses!

--- Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/controller/HobbitController.java
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/controller/HobbitController.java
@@ -1,50 +1,48 @@
 package com.qa.springstarter.controller;
 
+import com.qa.springstarter.service.HobbitService;
 import com.qa.springstarter.shire.Hobbit;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class HobbitController {
 
-    List<Hobbit> hobbits = new ArrayList<>();
+    private HobbitService service;
+
+    public HobbitController(HobbitService service) {
+        this.service = service;
+    }
 
     @PostMapping("/hobbit/create")
     public ResponseEntity<Hobbit> createHobbit(@RequestBody Hobbit hobbit) {
-        this.hobbits.add(hobbit);
-        Hobbit created = this.hobbits.get(this.hobbits.size() - 1);
+        Hobbit created = service.createHobbit(hobbit);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
     @GetMapping("/hobbit/list-hobbits")
-    public ResponseEntity<List<Hobbit>> getHobbits() {
-        return new ResponseEntity<>(hobbits, HttpStatus.OK);
+    public List<Hobbit> getHobbits() {
+        return service.getAllHobbits();
     }
 
     @GetMapping("/hobbit/list-hobbit/{id}")
-    public ResponseEntity<Hobbit> getHobbit(@PathVariable("id") int id) {
-        Hobbit myHobbit = this.hobbits.get(id);
-        return new ResponseEntity<>(myHobbit, HttpStatus.OK);
+    public Hobbit getHobbit(@PathVariable("id") int id) {
+        return service.getAHobbit(id);
     }
 
     @PatchMapping("hobbit/update/{id}")
     public Hobbit updateHobbit(@PathVariable("id") int id, @PathParam("forename") String forename,
                                @PathParam("familyName") String familyName, @PathParam("age") int age) {
-        Hobbit updatedHobbit = hobbits.get(id);
-        updatedHobbit.setForename(forename);
-        updatedHobbit.setFamilyName(familyName);
-        updatedHobbit.setAge(age);
-        return updatedHobbit;
+        return service.updateHobbit(id, forename, familyName, age);
     }
 
     @DeleteMapping("hobbit/delete/{id}")
     public ResponseEntity<?> deleteHobbit(@PathVariable("id") int id) {
-        hobbits.remove(id);
+        service.deleteHobbit(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
