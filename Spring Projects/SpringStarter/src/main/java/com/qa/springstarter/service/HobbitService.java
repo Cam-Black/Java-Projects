3e92492fbaf5451812fab8b178981933cc651cfa
Cam-Black <cameronablack1998@gmail.com>
2022-07-05 15:57:01 +0100

    Created new Interface and class files to move CRUD functionality into a service.

---
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/service/HobbitService.java
@@ -0,0 +1,5 @@
+package com.qa.springstarter.service;
+
+public interface HobbitService {
+
+}
---
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/shire/service/HobbitServiceList.java
@@ -0,0 +1,7 @@
+package com.qa.springstarter.shire.service;
+
+import com.qa.springstarter.service.HobbitService;
+
+public class HobbitServiceList implements HobbitService {
+
+}
