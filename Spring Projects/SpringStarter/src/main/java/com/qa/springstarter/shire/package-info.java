f9896336db08ce6dd8bf5c4c6933cb9433a7621a
Cam-Black <cameronablack1998@gmail.com>
2022-07-06 10:14:25 +0100

    Added Javadocs for package com.qa.springstarter.shire.

---
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/shire/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for holding Class files needed for populating your own Shire.<br>
+ * The Class files contain the ability to create your own Hobbit.
+ *
+ * @see com.qa.springstarter.shire.Hobbit Hobbit Class.
+ * @see com.qa.springstarter.shire.service.HobbitServiceList Hobbit CRUD Functions.
+ */
+package com.qa.springstarter.shire;
