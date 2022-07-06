343e2be03cd9d268a2f8f8dc6c8e7f97c2497c84
Cam-Black <cameronablack1998@gmail.com>
2022-07-06 11:51:46 +0100

    Created HobbitServiceDB Class to implement Service interface to interact with a DB instead of a List.

---
+++ Spring Projects/SpringStarter/src/main/java/com/qa/springstarter/shire/service/HobbitServiceDB.java
@@ -0,0 +1,50 @@
+package com.qa.springstarter.shire.service;
+
+import com.qa.springstarter.service.HobbitService;
+import com.qa.springstarter.shire.Hobbit;
+import com.qa.springstarter.shire.repository.HobbitRepo;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Primary
+public class HobbitServiceDB implements HobbitService {
+
+    private HobbitRepo repo;
+
+    public HobbitServiceDB(HobbitRepo repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public Hobbit getAHobbit(int id) {
+        return repo.findById(id).get();
+    }
+
+
+    @Override
+    public List<Hobbit> getAllHobbits() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Hobbit createHobbit(Hobbit hobbit) {
+        return repo.save(hobbit);
+    }
+
+    @Override
+    public Hobbit updateHobbit(int id, String forename, String familyName, int age) {
+        Hobbit toUpdate = repo.findById(id).get();
+        toUpdate.setForename(forename);
+        toUpdate.setFamilyName(familyName);
+        toUpdate.setAge(age);
+        return repo.save(toUpdate);
+    }
+
+    @Override
+    public void deleteHobbit(int id) {
+        repo.deleteById(id);
+    }
+}
