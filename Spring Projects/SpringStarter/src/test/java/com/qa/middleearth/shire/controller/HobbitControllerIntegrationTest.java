57532f070516d50e5b6e9d7780d0e626a70ca400
Cam-Black <cameronablack1998@gmail.com>
2022-07-07 11:22:05 +0100

    Created passing tests for full coverage on the HobbitController.

--- Spring Projects/SpringStarter/src/test/java/com/qa/middleearth/shire/controller/HobbitControllerIntegrationTest.java
+++ Spring Projects/SpringStarter/src/test/java/com/qa/middleearth/shire/controller/HobbitControllerIntegrationTest.java
@@ -7,15 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = {"classpath:hobbit-schema.sql",
+        "classpath:hobbit-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class HobbitControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
@@ -27,14 +32,53 @@ public class HobbitControllerIntegrationTest {
     void testCreate() throws Exception {
         Hobbit testHobbit = new Hobbit("Frodo", "Baggins", 50);
         String testHobbitAsJSON = this.mapper.writeValueAsString(testHobbit);
-        RequestBuilder req = post("/hobbit/create")
-                .content(testHobbitAsJSON)
-                .contentType(MediaType.APPLICATION_JSON);
-        ResultMatcher checkStatus = MockMvcResultMatchers.status().is(201);
-        Hobbit createdHobbit = new Hobbit(1, "Frodo", "Baggins", 50);
-        String createdDinoAsJSON = this.mapper.writeValueAsString(createdHobbit);
-        ResultMatcher checkBody = MockMvcResultMatchers.content().json(createdDinoAsJSON);
+        Hobbit createdHobbit = new Hobbit(2, "Frodo", "Baggins", 50);
+        String createdHobbitAsJSON = this.mapper.writeValueAsString(createdHobbit);
 
-        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+        this.mvc.perform(post("/hobbit/create")
+                        .content(testHobbitAsJSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(createdHobbitAsJSON));
+    }
+
+    @Test
+    void testGetHobbits() throws Exception {
+        List<Hobbit> hobbits = new ArrayList<>();
+        hobbits.add(new Hobbit(1, "Bilbo", "Baggins", 150));
+        String listToJSON = this.mapper.writeValueAsString(hobbits);
+        this.mvc.perform(get("/hobbit/list-hobbits")).andExpect(content().json(listToJSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetAHobbit() throws Exception {
+        Hobbit hobbit = new Hobbit(1, "Bilbo", "Baggins", 150);
+        String hobbitToJSON = this.mapper.writeValueAsString(hobbit);
+        this.mvc.perform(get("/hobbit/list-hobbit/1")).andExpect(content().json(hobbitToJSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        Hobbit updatedHobbit = new Hobbit(1, "Scarlet", "Baggins", 150);
+        String toJSON = this.mapper.writeValueAsString(updatedHobbit);
+        this.mvc.perform(patch("/hobbit/update/1?forename=Scarlet&familyName=Baggins&age=150"))
+                .andExpect(content().json(toJSON)).andExpect(status().isOk());
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        this.mvc.perform(delete("/hobbit/delete/1")).andExpect(status().isNoContent());
+    }
+
+    @Test
+    void testGetByFamilyName() throws Exception {
+        List<Hobbit> baggins = new ArrayList<>();
+        baggins.add(new Hobbit(1, "Bilbo", "Baggins", 150));
+        String bagginsToJSON = this.mapper.writeValueAsString(baggins);
+        this.mvc.perform(get("/hobbit/findFamilies/?familyName=Baggins"))
+                .andExpect(content().json(bagginsToJSON))
+                .andExpect(status().isOk());
     }
 }
