1ad9b67b9da441a466ce7131216d4bc9be29b12d
Cam-Black <cameronablack1998@gmail.com>
2022-09-13 12:13:17 +0100

    Added private fields along with a default constructor.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/LearningPackage.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/LearningPackage.java
@@ -1,4 +1,13 @@
 package com.qa.cloudacademy.sectionthree.learningpackage;
 
 public class LearningPackage {
+	private int learningPackageLevel;
+	private int numOfCourses;
+	private int costPerCourse;
+	
+	public LearningPackage() {
+		super();
+	}
+	
+	
 }
