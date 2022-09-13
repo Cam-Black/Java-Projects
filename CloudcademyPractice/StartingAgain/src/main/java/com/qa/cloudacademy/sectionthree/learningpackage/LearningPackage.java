3f900ee324d82412ea9694a570ba131b435c53ce
Cam-Black <cameronablack1998@gmail.com>
2022-09-13 12:31:20 +0100

    Added instance of Utils class and method to get the package level from user input.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/LearningPackage.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/LearningPackage.java
@@ -4,10 +4,15 @@ public class LearningPackage {
 	private int learningPackageLevel;
 	private int numOfCourses;
 	private int costPerCourse;
+	private final Utils UTILS;
 	
 	public LearningPackage() {
 		super();
+		UTILS = new Utils();
 	}
 	
+	public void setLearningPackageLevelUserInput(int learningPackageLevel) {
+		this.learningPackageLevel = UTILS.getInt();
+	}
 	
 }
