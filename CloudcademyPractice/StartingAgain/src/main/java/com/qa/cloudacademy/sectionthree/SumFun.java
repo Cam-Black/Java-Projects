652ad16ed7f743c1575de455b6e7770d2846f89e
Cam-Black <cameronablack1998@gmail.com>
2022-09-13 11:19:30 +0100

    Using while loop, printing out the value of sum at each iteration, printing out the same text each time to prompt for user input

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/SumFun.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/SumFun.java
@@ -4,18 +4,24 @@ import java.util.Scanner;
 
 public class SumFun {
 	public static void main(String[] args) {
-		int sum = 0;
 		Scanner s = new Scanner(System.in);
+		int sum = 0;
 		int input;
+		
 		System.out.println("Enter a positive integer number to add to the sum");
-		System.out.println("or enter a negative integer number to print the sum and exit the app.");
+		System.out.println("Enter a negative integer number to print the sum and exit the app.");
 		input = s.nextInt();
+		
 		while (input >= 0) {
 			sum += input;
-			System.out.println("Enter next number: ");
+			System.out.println("sum = " + sum);
+			System.out.println();
+			System.out.println("Enter a positive integer number to add to the sum");
+			System.out.println("Enter a negative integer number to print the sum and exit the app.");
 			input = s.nextInt();
 		}
-		System.out.println("Sum is: " + sum);
+		
+		System.out.println("Final sum = " + sum);
 		System.out.println("Goodbye!");
 	}
 }
