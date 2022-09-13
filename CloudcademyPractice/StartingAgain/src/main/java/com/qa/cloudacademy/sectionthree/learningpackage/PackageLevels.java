c6d2131d76364c1f8d9826381f00be7a483be4f1
Cam-Black <cameronablack1998@gmail.com>
2022-09-13 12:56:22 +0100

    Added methods to get information for a package, and print all packages to the console.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/PackageLevels.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/PackageLevels.java
@@ -6,12 +6,24 @@ public enum PackageLevels {
 	PACKAGE_THREE(6, 15.00, 3.00);
 	
 	private final int numOfIncludedCourses;
-	private final double coursePrice;
+	private final double packagePrice;
 	private final double additionalCourseCost;
 	
-	PackageLevels(int numOfIncludedCourses, double coursePrice, double additionalCourseCost) {
+	PackageLevels(int numOfIncludedCourses, double packagePrice, double additionalCourseCost) {
 		this.numOfIncludedCourses = numOfIncludedCourses;
-		this.coursePrice = coursePrice;
+		this.packagePrice = packagePrice;
 		this.additionalCourseCost = additionalCourseCost;
+	}
+	
+	public String packageInfo() {
+		return this.name() + "[Number of Courses included: " + this.numOfIncludedCourses + ", Package price: $" +
+				String.format("%.2f", this.packagePrice) + ", Additional course cost: $" +
+				String.format("%.2f", this.additionalCourseCost);
+	}
+	
+	public static void printPackages() {
+		for (PackageLevels packageLevels : PackageLevels.values()) {
+			System.out.println(packageLevels.packageInfo());
+		}
 	}
 }
