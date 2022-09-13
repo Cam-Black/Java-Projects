f210d2ad0114306aac50ca6bd24fb8e39afbe813
Cam-Black <cameronablack1998@gmail.com>
2022-09-13 09:50:14 +0100

    Completed MadLibs project.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/MadLibs.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/MadLibs.java
@@ -6,7 +6,45 @@ public class MadLibs {
 	public static void main(String[] args) {
 		String adjOne, girlsName, adjTwo, occOne, kingdom, clothing, hobby, adjThree, occTwo, boysName, father;
 		Scanner s = new Scanner(System.in);
-		System.out.println("Type an adjective:");
 		
+		System.out.println("Type an adjective:");
+		adjOne = s.nextLine();
+		
+		System.out.println("Next, type a girls name:");
+		girlsName = s.nextLine();
+		
+		System.out.println("Now another adjective:");
+		adjTwo = s.nextLine();
+		
+		System.out.println("Then an occupation:");
+		occOne = s.nextLine();
+		
+		System.out.println("Now we need a location:");
+		kingdom = s.nextLine();
+		
+		System.out.println("And type of clothing:");
+		clothing = s.nextLine();
+		
+		System.out.println("Now for a hobby:");
+		hobby = s.nextLine();
+		
+		System.out.println("A final adjective:");
+		adjThree = s.nextLine();
+		
+		System.out.println("Another occupation:");
+		occTwo = s.nextLine();
+		
+		System.out.println("A name for a boy:");
+		boysName = s.nextLine();
+		
+		System.out.println("And finally a name for a man:");
+		father = s.nextLine();
+		
+		String story =
+				"There once was a " + adjOne + " girl named " + girlsName + " who was a " + adjTwo + " " + occOne +
+						" in the " + "kingdom" + " of " + kingdom + ".\nShe loved to wear some sort of " +
+						clothing + " and to " + hobby + ".\nShe wanted to marry the " + adjThree + " " + occTwo +
+						" named " + boysName + ".\nBut her father, King " + father + " forbid her from seeing him.";
+		System.out.println(story);
 	}
 }
