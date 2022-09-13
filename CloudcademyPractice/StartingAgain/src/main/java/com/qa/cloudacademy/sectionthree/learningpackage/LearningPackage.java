6d8a095d610f61afd6a2f2d609cdc9edbd99c631
Cam-Black <cameronablack1998@gmail.com>
2022-09-13 13:28:22 +0100

    Overridden toString() method, Changed getters and setters to appropriate return types, (ints to doubles for cost), finished switch statement to set values of LearningPackage based on user input.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/LearningPackage.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionthree/learningpackage/LearningPackage.java
@@ -2,8 +2,9 @@ package com.qa.cloudacademy.sectionthree.learningpackage;
 
 public class LearningPackage {
 	private int learningPackageLevel;
+	private double packageCost;
 	private int numOfCourses;
-	private int costPerCourse;
+	private double costPerCourse;
 	private final Utils UTILS;
 	
 	public LearningPackage() {
@@ -11,8 +12,70 @@ public class LearningPackage {
 		UTILS = new Utils();
 	}
 	
-	public void setLearningPackageLevelUserInput(int learningPackageLevel) {
-		this.learningPackageLevel = UTILS.getInt();
+	public int getLearningPackageLevel() {
+		return learningPackageLevel;
 	}
 	
+	public void setLearningPackageLevel(int learningPackageLevel) {
+		this.learningPackageLevel = learningPackageLevel;
+	}
+	
+	public double getPackageCost() {
+		return packageCost;
+	}
+	
+	public void setPackageCost(double packageCost) {
+		this.packageCost = packageCost;
+	}
+	
+	public int getNumOfCourses() {
+		return numOfCourses;
+	}
+	
+	public void setNumOfCourses(int numOfCourses) {
+		this.numOfCourses = numOfCourses;
+	}
+	
+	public double getCostPerCourse() {
+		return costPerCourse;
+	}
+	
+	public void setCostPerCourse(double costPerCourse) {
+		this.costPerCourse = costPerCourse;
+	}
+	
+	public void setLearningPackageLevelUserInput() {
+		System.out.println("Please enter the number of the package you would like to select");
+		setLearningPackageLevel(UTILS.getInt());
+		switch (this.learningPackageLevel) {
+			case 1:
+				this.setPackageCost(PackageLevels.PACKAGE_ONE.getPackagePrice());
+				this.setNumOfCourses(PackageLevels.PACKAGE_ONE.getNumOfIncludedCourses());
+				this.setCostPerCourse(PackageLevels.PACKAGE_ONE.getAdditionalCourseCost());
+				break;
+				
+			case 2:
+				this.setPackageCost(PackageLevels.PACKAGE_TWO.getPackagePrice());
+				this.setNumOfCourses(PackageLevels.PACKAGE_TWO.getNumOfIncludedCourses());
+				this.setCostPerCourse(PackageLevels.PACKAGE_TWO.getAdditionalCourseCost());
+				break;
+				
+			case 3:
+				this.setPackageCost(PackageLevels.PACKAGE_THREE.getPackagePrice());
+				this.setNumOfCourses(PackageLevels.PACKAGE_THREE.getNumOfIncludedCourses());
+				this.setCostPerCourse(PackageLevels.PACKAGE_THREE.getAdditionalCourseCost());
+				break;
+				
+			default:
+				System.out.println("This is not a valid package level!");
+				break;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Package Level: " + this.getLearningPackageLevel() + "\nPackage Cost: " + this.getPackageCost() +
+				"\nNumber of " + "included courses: " + this.getNumOfCourses() + "\nCost per additional cours: " +
+				this.getCostPerCourse() + "]";
+	}
 }
