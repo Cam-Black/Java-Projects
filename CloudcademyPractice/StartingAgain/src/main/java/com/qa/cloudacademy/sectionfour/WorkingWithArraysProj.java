702605bcb00a7c91088f2587a4e8242dbb001cdc
Cam-Black <cameronablack1998@gmail.com>
2022-09-14 12:42:27 +0100

    Created functionality allowing users to input 5 integers, add them to the array, then print out the element multiplied by 2.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionfour/WorkingWithArraysProj.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionfour/WorkingWithArraysProj.java
@@ -1,4 +1,25 @@
 package com.qa.cloudacademy.sectionfour;
 
+import java.util.Scanner;
+
+//Prompt User for input for 5 integers
+//Store each integer in a regular, built in array
+//print each element multiplied by 2
+
 public class WorkingWithArraysProj {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in); //Create new scanner obj for user input
+		Integer[] nums = new Integer[5];
+		int i = 0;
+		while (i < nums.length) {
+			System.out.print("Enter an integer:\t");
+			nums[i] = s.nextInt();
+//			s.nextLine();
+			i++;
+		}
+		for (int num : nums) {
+			System.out.println(num + " * 2" + " = " + (num * 2));
+		}
+		
+	}
 }
