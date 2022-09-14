b33175d292359b7f32de9028dcc1948a48ae6a02
Cam-Black <cameronablack1998@gmail.com>
2022-09-14 15:52:44 +0100

    Completed challenge, add user input to the List, then print to the console using a stream. Also used a for loop to just see the difference between syntax.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionfour/WorkingWithArrayListsProj.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionfour/WorkingWithArrayListsProj.java
@@ -1,5 +1,26 @@
 package com.qa.cloudacademy.sectionfour;
 
-public class WorkingWithArrayListsProj {
+import java.util.*;
 
+public class WorkingWithArrayListsProj {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		List nums = new ArrayList<>();
+		double userNum;
+		do {
+			System.out.println("Enter a negative integer to exit");
+			System.out.println("Enter a positive integer to add it to the list");
+			userNum = s.nextDouble();
+			if (userNum >= 0) {
+				nums.add(userNum);
+			}
+		} while (userNum >= 0);
+		
+		//Using a for-loop to reverse the order of the array list and print to screen
+//		for (int i = nums.size() - 1; i >= 0; i--) {
+//			System.out.println(nums.get(i));
+//		}
+		//Reverse and print order using a stream.
+		nums.stream().sorted(Collections.reverseOrder()).forEach((el) -> System.out.println(el));
+	}
 }
