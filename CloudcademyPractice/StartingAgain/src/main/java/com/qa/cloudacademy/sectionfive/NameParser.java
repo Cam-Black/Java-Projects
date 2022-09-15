9fef4e81f0041a2d877d5111d50c5a76e2a64d82
Cam-Black <cameronablack1998@gmail.com>
2022-09-15 09:44:39 +0100

    Created a class for taking user input and then splitting the string to return first and last name separately. Added variables.

---
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionfive/NameParser.java
@@ -0,0 +1,16 @@
+package com.qa.cloudacademy.sectionfive;
+
+import java.util.Scanner;
+
+public class NameParser {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String name;
+		String firstName;
+		String lastName;
+		
+		
+	}
+	
+	
+}
