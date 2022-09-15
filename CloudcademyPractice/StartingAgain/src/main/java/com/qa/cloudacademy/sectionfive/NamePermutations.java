657e42c824fc5ff23d7cf0b5e57da4b58d949cc5
Cam-Black <cameronablack1998@gmail.com>
2022-09-15 11:13:06 +0100

    Completed challenge, using a nested enhanced for loop to get every permutation

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionfive/NamePermutations.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionfive/NamePermutations.java
@@ -1,7 +1,34 @@
 package com.qa.cloudacademy.sectionfive;
 
+import com.qa.cloudacademy.sectionthree.learningpackage.PackageLevels;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Prompt user for 5 full names.
+ * Separate first and last name, storing them in separate ArrayLists.
+ * Print out each first name with each last name for 25 possible combinations.
+ */
+
 public class NamePermutations {
 	public static void main(String[] args) {
-	
+		Scanner s = new Scanner(System.in);
+		List<String> forenames = new ArrayList<>();
+		List<String> surnames = new ArrayList<>();
+		String name;
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Enter a person's name: ");
+			name = s.nextLine();
+			forenames.add(name.substring(0, name.indexOf(" ")));
+			surnames.add(name.substring(name.indexOf(" ") + 1));
+		}
+		for (String forename : forenames) {
+			for (String surname : surnames) {
+				System.out.println(forename + " " + surname);
+			}
+		}
 	}
 }
