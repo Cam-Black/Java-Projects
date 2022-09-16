dc8806a11bfd56e4cb29853675499367bb45bf4d
Cam-Black <cameronablack1998@gmail.com>
2022-09-16 11:09:35 +0100

    Created method to fill an Integer List from a passed int, a method for printing all values to the console using a stream, and the reduce stream to get the sum of all elements in the List.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionsix/SumOfElements.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionsix/SumOfElements.java
@@ -1,5 +1,31 @@
 package com.qa.cloudacademy.sectionsix;
 
-public class SumOfElements {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class SumOfElements {
+	public static void main(String[] args) {
+		List numList = new ArrayList<>();
+		fillList(numList, 5);
+		printList(numList);
+		System.out.println();
+		System.out.println(sumElements(numList));
+	}
+	
+	public static int sumElements(List<Integer> numList) {
+		return numList.stream().reduce((acc, next) -> acc + next).get();
+	}
+	
+	public static List<Integer> fillList(List<Integer> numList, int listSize) {
+		Random rand = new Random();
+		for (int i = 0; i < listSize; i++) {
+			numList.add(rand.nextInt(100));
+		}
+		return numList;
+	}
+	
+	public static void printList(List<Integer> numList) {
+		numList.forEach(System.out::println);
+	}
 }
