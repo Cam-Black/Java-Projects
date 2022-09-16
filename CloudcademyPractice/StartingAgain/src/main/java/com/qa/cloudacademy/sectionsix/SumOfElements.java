e05952cf59a531aa2c8fec196b252b39e1155eb6
Cam-Black <cameronablack1998@gmail.com>
2022-09-16 11:22:54 +0100

    Refactored adding data type to List in main, changed stream to just calculate sum without creating the variables first, changed fillList to create a new ArrayList with size determined by parameter int listSize.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionsix/SumOfElements.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionsix/SumOfElements.java
@@ -6,23 +6,24 @@ import java.util.Random;
 
 public class SumOfElements {
 	public static void main(String[] args) {
-		List numList = new ArrayList<>();
-		fillList(numList, 5);
+		List<Integer> numList = new ArrayList<>();
+		createAndFillList(5);
 		printList(numList);
 		System.out.println();
 		System.out.println(sumElements(numList));
 	}
 	
 	public static int sumElements(List<Integer> numList) {
-		return numList.stream().reduce((acc, next) -> acc + next).get();
+		return numList.stream().reduce(Integer::sum).get();
 	}
 	
-	public static List<Integer> fillList(List<Integer> numList, int listSize) {
+	public static List<Integer> createAndFillList(int listSize) {
+		List<Integer> nums = new ArrayList<>();
 		Random rand = new Random();
 		for (int i = 0; i < listSize; i++) {
-			numList.add(rand.nextInt(100));
+			nums.add(rand.nextInt(100));
 		}
-		return numList;
+		return nums;
 	}
 	
 	public static void printList(List<Integer> numList) {
