df1a30b4baa720a20becb7d64f4c414a5538fcd4
Cam-Black <cameronablack1998@gmail.com>
2022-09-16 09:49:41 +0100

    Added try catch to getResult(int, String) to ensure execution doesn't exit if invalid data is passed in as a String. Added other overloaded methods.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionsix/MethodOverloading.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionsix/MethodOverloading.java
@@ -1,4 +1,36 @@
 package com.qa.cloudacademy.sectionsix;
 
+import java.util.Scanner;
+
 public class MethodOverloading {
+	public static final Scanner SCAN = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println(getResult(2, "d"));
+	}
+	
+	
+	static int getResult(int num) {
+		return num + num;
+	}
+	
+	static int getResult(int num, int numTwo) {
+		return num * numTwo;
+	}
+	
+	static int getResult(int num, String numTwo) {
+		int numFromString = 0;
+		boolean yOrN = true;
+		while (yOrN) {
+			try {
+				numFromString = Integer.parseInt(numTwo);
+				yOrN = false;
+			} catch (NumberFormatException nfe) {
+				System.err.println("Unable to convert String " + numTwo + " to integer.");
+				System.out.println("Try entering a new value:");
+				numTwo = SCAN.nextLine();
+			}
+		}
+		return num + numFromString;
+	}
 }
