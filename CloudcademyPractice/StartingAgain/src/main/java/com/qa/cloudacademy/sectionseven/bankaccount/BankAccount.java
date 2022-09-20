9f451644fb2ab37e70b883ba0f705f30d3891b55
Cam-Black <cameronablack1998@gmail.com>
2022-09-20 15:00:55 +0100

    Created functionality for creating BankAccount objects with ability to withdraw and deposit to it, making owner a read only field. Tested in main to check outputs are as expected.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionseven/bankaccount/BankAccount.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionseven/bankaccount/BankAccount.java
@@ -1,5 +1,61 @@
 package com.qa.cloudacademy.sectionseven.bankaccount;
 
 public class BankAccount {
-
+	private String owner;
+	private int balance;
+	
+	public BankAccount() {
+		super();
+		this.balance = 0;
+	}
+	
+	public BankAccount(String owner) {
+		this();
+		this.owner = owner;
+	}
+	
+	public BankAccount(String owner, int balance) {
+		this(owner);
+		if (balance <= 0) {
+			System.err.println("Invalid amount, balance not set!");
+		} else {
+			this.balance = balance;
+			System.out.println("Balance Set!");
+			System.out.println();
+		}
+	}
+	
+	public void deposit(int amount) {
+		if (amount < 0){
+			System.err.println("Can not add 0 or less to bank account, try again!");
+			System.out.println();
+		} else {
+			this.balance += amount;
+			System.out.println(this.getOwner() + "'s balance is now: $" + this.getBalance());
+			System.out.println();
+		}
+	}
+	
+	public void withdraw(int amount) {
+		if (amount > this.balance) {
+			System.err.println("Funds unavailable.");
+			System.err.println("Current funds: $" + this.balance);
+			System.out.println();
+		} else {
+			this.balance -= amount;
+		}
+	}
+	
+	public String getOwner() {
+		return this.owner;
+	}
+	
+	public int getBalance() {
+		return this.balance;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account[Owner: " + this.getOwner() + ", Balance = $" + this.getBalance();
+	}
 }
--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionseven/bankaccount/Runner.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionseven/bankaccount/Runner.java
@@ -1,4 +1,13 @@
 package com.qa.cloudacademy.sectionseven.bankaccount;
 
 public class Runner {
+	public static void main(String[] args) {
+		BankAccount myAccount = new BankAccount("Cameron", 2000);
+		myAccount.withdraw(500);
+		myAccount.deposit(100);
+		BankAccount yourAccount = new BankAccount("Joe", 500);
+		
+		System.out.println(myAccount);
+		System.out.println(yourAccount);
+	}
 }
