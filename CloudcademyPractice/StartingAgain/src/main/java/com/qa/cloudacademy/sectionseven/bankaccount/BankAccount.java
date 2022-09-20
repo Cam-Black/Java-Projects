d6344ebb65aa48e0a252873a45f9fcd1aa2f640a
Cam-Black <cameronablack1998@gmail.com>
2022-09-20 14:36:51 +0100

    Added Class files for completing Bank Account project on Cloud Academy.

---
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionseven/bankaccount/BankAccount.java
@@ -0,0 +1,5 @@
+package com.qa.cloudacademy.sectionseven.bankaccount;
+
+public class BankAccount {
+
+}
---
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectionseven/bankaccount/Runner.java
@@ -0,0 +1,4 @@
+package com.qa.cloudacademy.sectionseven.bankaccount;
+
+public class Runner {
+}
