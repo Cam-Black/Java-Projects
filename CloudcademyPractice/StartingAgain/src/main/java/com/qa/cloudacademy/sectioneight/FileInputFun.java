61bf4936f772b5d81c973c88b8e6f1b2cc44d27a
Cam-Black <cameronablack1998@gmail.com>
2022-09-21 09:25:26 +0100

    Created try-catch block to read from a file.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/FileInputFun.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/FileInputFun.java
@@ -1,5 +1,24 @@
 package com.qa.cloudacademy.sectioneight;
 
-public class FileInputFun {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class FileInputFun {
+	public static void main(String[] args) {
+		Scanner s;
+		
+		try {
+			s = new Scanner(new File("./src/main/resources/file.txt"));
+			int input;
+			
+			while (s.hasNext()) {
+				input = s.nextInt();
+				System.out.println(input);
+			}
+		} catch (FileNotFoundException fnfe) {
+			System.err.println("Can't find file:");
+			System.err.println(fnfe.getMessage());
+		}
+	}
 }
