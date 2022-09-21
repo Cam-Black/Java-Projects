8e9eb6e47466692df0513501a68fd5197e6f85b4
Cam-Black <cameronablack1998@gmail.com>
2022-09-21 12:13:45 +0100

    Added files needed for Circle Project on Cloud Academy.

---
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/circle/Circle.java
@@ -0,0 +1,40 @@
+package com.qa.cloudacademy.sectioneight.circle;
+
+public class Circle {
+	private double radius;
+	
+	public Circle() {
+		super();
+		this.radius = 1.0;
+	}
+	
+	public Circle(double radius) {
+		super();
+		this.radius = radius;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	public double circumference() {
+		return (Math.PI * this.getRadius()) * 2;
+	}
+	
+	public double area() {
+		return Math.PI * Math.pow(this.getRadius(), 2);
+	}
+	
+	public String formatResult(double result) {
+		return String.format("%.2f", result);
+	}
+	@Override
+	public String toString() {
+		return "Radius = " + formatResult(this.getRadius()) + "\nCircumference = " + this.formatResult(circumference())
+				+ "\nArea = " + formatResult(this.area());
+	}
+}
---
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/circle/CircleFromFile.java
@@ -0,0 +1,4 @@
+package com.qa.cloudacademy.sectioneight.circle;
+
+public class CircleFromFile {
+}
