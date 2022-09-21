093daae2879b255f1eda9e2318c96c4ff5053384
Cam-Black <cameronablack1998@gmail.com>
2022-09-21 12:29:11 +0100

    Completed code to take in a Circle object, add it to an array list, then print that array list to the console along with writing the data to a file.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/circle/CircleFromFile.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/circle/CircleFromFile.java
@@ -1,4 +1,53 @@
 package com.qa.cloudacademy.sectioneight.circle;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class CircleFromFile {
+	private final String inputFilePath;
+	private final String outputFilePath;
+	
+	public CircleFromFile(String filePath, String outputFilePath) {
+		super();
+		this.inputFilePath = filePath;
+		this.outputFilePath = outputFilePath;
+	}
+	
+	public List<Circle> createCircles() {
+		List<Circle> circles = new ArrayList<>();
+		try {
+			Scanner s = new Scanner(new File(inputFilePath));
+			double tempR;
+			while (s.hasNext()) {
+				tempR = s.nextDouble();
+				Circle circ = new Circle(tempR);
+				circles.add(circ);
+			}
+		} catch (FileNotFoundException fnfe) {
+			System.err.println("File not found:");
+			System.err.println(fnfe.getMessage());
+		} catch (InputMismatchException ime) {
+			System.err.println("Not a valid number!");
+		}
+		return circles;
+	}
+	
+	public void printCircles(List<Circle> circles) {
+		try (PrintWriter pw = new PrintWriter(outputFilePath)) {
+			circles.forEach((el) -> {
+				System.out.println(el);
+				pw.println(el);
+				pw.println();
+				System.out.println();
+			});
+		} catch (FileNotFoundException fnfe) {
+			System.err.println("File not found:");
+			System.err.println(fnfe.getMessage());
+		}
+	}
 }
--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/circle/Runner.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/circle/Runner.java
@@ -1,7 +1,11 @@
 package com.qa.cloudacademy.sectioneight.circle;
 
+import java.util.List;
+
 public class Runner {
 	public static void main(String[] args) {
-		
+		CircleFromFile cff = new CircleFromFile("circle_data.txt", "circle_output.txt");
+		List<Circle> circles = cff.createCircles();
+		cff.printCircles(circles);
 	}
 }
