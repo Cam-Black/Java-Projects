f42db6b9af2adf5db7ab976e1b3c176abc3c412a
Cam-Black <cameronablack1998@gmail.com>
2022-09-21 09:35:50 +0100

    Added a counter to display what line of file is being printed, allowing for easier troubleshooting. Added InputMismatchException to a separate catch block.

--- CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/FileInputFun.java
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/FileInputFun.java
@@ -2,23 +2,27 @@ package com.qa.cloudacademy.sectioneight;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FileInputFun {
 	public static void main(String[] args) {
 		Scanner s;
+		int i = 1;
 		
 		try {
 			s = new Scanner(new File("./src/main/resources/file.txt"));
 			int input;
-			
 			while (s.hasNext()) {
 				input = s.nextInt();
-				System.out.println(input);
+				System.out.println(i + " - " + input);
+				i++;
 			}
 		} catch (FileNotFoundException fnfe) {
 			System.err.println("Can't find file:");
 			System.err.println(fnfe.getMessage());
+		} catch (InputMismatchException ime) {
+			System.err.println(i + " - Not a valid Integer!");
 		}
 	}
 }
