4d2fda981fb9b42d2c874775e91c2a02739de956
Cam-Black <cameronablack1998@gmail.com>
2022-09-21 09:58:05 +0100

    Created files for testing writing to a file using print writer.

---
+++ CloudcademyPractice/StartingAgain/src/main/java/com/qa/cloudacademy/sectioneight/FileOutputFun.java
@@ -0,0 +1,30 @@
+package com.qa.cloudacademy.sectioneight;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileOutputFun {
+	public static void main(String[] args) {
+		Scanner s;
+		String output;
+		try {
+			PrintWriter pw = new PrintWriter("./src/main/resources/output.txt");
+			
+			pw.println("Hello There!");
+			pw.println("My name is Cameron Black");
+			s = new Scanner(new File("./src/main/resources/output.txt"));
+			
+			while (s.hasNext()) {
+				output = s.nextLine();
+				System.out.println(output);
+			}
+			pw.close();
+			
+		} catch (FileNotFoundException fnfe) {
+			System.err.println("File not found:");
+			System.err.println(fnfe.getMessage());
+		}
+	}
+}
